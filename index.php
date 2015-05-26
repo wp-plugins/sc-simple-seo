fab31ed010e51c9a027972d67441d73d916f0a69
sergiuscosta <sergiuscosta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 12:57:06 +0000

    added to analytics code
    git-svn-id: https://plugins.svn.wordpress.org/sc-simple-seo/trunk@1167669 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -215,18 +215,18 @@ add_action('wp_head', 'inserthead');
 /*
  * Google Analytics
  */
-function google_analytics() { ?>
-<script type='text/javascript'>
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', '<?php echo $options["scseo_analytics"]; ?>']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
-<?php }
+function google_analytics() { 
+    $options = get_option( 'scseo_settings' ); ?>
+    <script type='text/javascript'>
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '<?php echo $options["scseo_analytics"]; ?>']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script> <?php }
 add_action('wp_footer', 'google_analytics'); 
 
 ?>
